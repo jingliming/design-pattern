e77c0c3605e5002e4cdfab60b2553e6524984f83
liming.jing <liming.jing@ele.me>
2020-03-15 23:06:37 +0800

    :construction: å­ : AOP

--- src/proxy/dynamicProxy/DynProxyLawyer.java
+++ src/proxy/dynamicProxy/DynProxyLawyer.java
@@ -2,6 +2,7 @@ package proxy.dynamicProxy;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class DynProxyLawyer implements InvocationHandler {
     private Object target;//被代理的对象
@@ -13,6 +14,8 @@ public class DynProxyLawyer implements InvocationHandler {
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         System.out.println("案件进展: " + method.getName());
+        if (method.getName().equals("submit"))
+            System.out.println("案件提交成功,证据如下："+ Arrays.asList(args));
         Object result = method.invoke(target, args);
         return result;
     }
